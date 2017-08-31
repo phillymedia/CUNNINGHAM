52aec8f736f670870365ad3a6526a5d01aabe2f9
jared-whalen <jwhalen@philly.com>
2017-08-31 09:46:08 -0400

    New text

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,10 @@
+require("/Users/whalenj/Documents/Projects/newsapps/CUNNINGHAM/app/js/typeit.min.js");
+
 $(document).ready(function(){
+  $('#example1').typeIt({
+     strings: 'This is a simple string.',
+     speed: 50,
+     autoStart: false,
+     cursor:false
+});
 });
